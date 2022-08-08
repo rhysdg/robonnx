daff6439dce91ee1709b6d8aaefe674d416f4186
Rhys Williams <rhysdgwilliams@gmail.com>
2022-08-08 21:22:50 +0100

    quick setup edit - package name, description

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as f:
+    long_description = f.read()
 setuptools.setup(
-    name="inference", 
+    name="robonnx", 
     version="0.0.1",
     author="Rhys Williams",
     author_email="rhysdgwilliams@gmail.com",
-    description="An onnx implementation of RoBERTa",
-    long_description="",
+    description="An example onnx implementation of RoBERTa for demonstration purposes",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusrname/projectnam",
+    url="https://github.com/https://github.com/rhysdg/robonnx",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
